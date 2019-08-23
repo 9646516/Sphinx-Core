1ae8009f2de0fc5425241e8723c25b209fe726d0
9646516 <zyq855@gmail.com>
2019-08-24 03:51:45 +0800

    add docker api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use shiplift::Docker;
+use shiplift::builder::ContainerListOptionsBuilder;
+use tokio::prelude::Future;
+use std::collections::HashMap;
+use std::string::String;
+
 fn main() {
-    println!("Hello, world!");
+    let docker = Docker::new();
+    let mut builder = ContainerListOptionsBuilder::default();
+    let fut = docker
+        .containers()
+        .list(&builder.all().build())
+        .map(|containers| {
+            for c in containers {
+                println!("container -> {:#?}", c)
+            }
+        })
+        .map_err(|e| eprintln!("Error: {}", e));
+
+    tokio::run(fut);
 }
