339dd46ef3fdeed798849d2f30928fa9374346b7
Myriad-Dreamin <camiyoru@gmail.com>
2020-09-26 04:34:35 +0800

    add compiler and judge trait

---
+++ sphinx-core-docker-provider/src/lib.rs
@@ -0,0 +1,11 @@
+pub use compiler::Compiler;
+pub use judge::Judge;
+pub use run::*;
+
+mod compiler;
+mod judge;
+mod env;
+mod utils;
+mod run;
+pub mod client;
+
---
+++ sphinx-core/src/lib.rs
@@ -0,0 +1,9 @@
+pub use compiler::*;
+pub use judge::*;
+pub use language::*;
+pub use problem_config::*;
+
+mod compiler;
+mod judge;
+mod language;
+mod problem_config;
